7c07fbb64ac598441da7d085bc7fcf56f3db234a
Sukhrob Yangibaev <sukhrobyangibaev@gmail.com>
2026-03-29 15:50:18 +0500

    Refactor code structure for improved readability and maintainability

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 import 'app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  
+  GoogleFonts.config.allowRuntimeFetching = false;
+
   if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
     sqfliteFfiInit();
     databaseFactory = databaseFactoryFfi;
   }
-  
+
   runApp(const BookAiApp());
 }
